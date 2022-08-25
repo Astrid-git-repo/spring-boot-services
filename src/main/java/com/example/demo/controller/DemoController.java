691638e3807470bf5ffa50203ad8738873bcc60c
ASTRID DCOSTA <astdcost@publicisgroupe.net>
2022-08-25 13:27:29 -0400

    test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class DemoController {
-	
+	Logger logger = LoggerFactory.getLogger(DemoController.class);
+
 	@GetMapping("/welcome")
 	  String welcomeMessage() {
 	    return "Welcome!!!!";
@@ -13,6 +16,7 @@ public class DemoController {
 	
 	@GetMapping("/welcome/{name}")
 	  public String welcomeMessage(@PathVariable String name) {
+		logger.info("Welcome Message --query param=",name);
 	    return "Hello There! Welcome "+name;
 	  }
 	
